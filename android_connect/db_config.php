6ab1f799346e5cced3fa26c07cefd9832d7e9474
Ciaran Park <ciaranpark@gmail.com>
2014-09-11 10:33:08 +0100

    Added PHP android connect layer

---
+++ android_connect/db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "user_glasstimote"); // db user
+define('DB_PASSWORD', "Glass789!"); // db password (mention your db password here)
+define('DB_DATABASE', "glasstimote"); // database name
+define('DB_SERVER', "192.168.10.110:3306"); // db server
+?>
---
+++ android_connect/get_ibeacon_details.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * Following code will get single ibeacon details
+ * A ibeacon is identified by ibeacon minor (minor)
+ */
+
+// array for JSON response
+$response = array();
+
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+
+// connecting to db
+$db = new DB_CONNECT();
+
+// check for post data
+if (isset($_GET["minor"])) {
+    $minor = $_GET['minor'];
+
+    // get a ibeacon from ibeacons table
+    $result = mysql_query("SELECT *FROM ibeacons WHERE minor = $minor");
+
+    if (!empty($result)) {
+        // check for empty result
+        if (mysql_num_rows($result) > 0) {
+
+            $result = mysql_fetch_array($result);
+
+            $ibeacon = array();
+            $ibeacon["pid"] = $result["pid"];
+            $ibeacon["name"] = $result["name"];
+            $ibeacon["colour"] = $result["colour"];
+            $ibeacon["address"] = $result["address"];
+            $ibeacon["major"] = $result["major"];
+            $ibeacon["minor"] = $result["minor"];
+            $ibeacon["image"] = $result["image"];
+            // success
+            $response["success"] = 1;
+
+            // user node
+            $response["ibeacon"] = array();
+
+            array_push($response["ibeacon"], $ibeacon);
+
+            // echoing JSON response
+            echo json_encode($response);
+        } else {
+            // no ibeacon found
+            $response["success"] = 0;
+            $response["message"] = "No ibeacon found";
+
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no ibeacon found
+        $response["success"] = 0;
+        $response["message"] = "No ibeacon found";
+
+        // echo no users JSON
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
